0052fc9cc22fdc720400ee7b8c2d496c54a9446f
Gabriela Dantas <gabicd3323@gmail.com>
2024-12-16 20:01:40 -0300

    html templates and page navigation

--- estoque_app/urls.py
+++ estoque_app/urls.py
@@ -6,11 +6,17 @@ urlpatterns = [
     path("", views.home, name="home"),
     
     # Criar usuário
-    path("createUser/", views.create_user, name="createUser"),
+    path("perfis/", views.perfis, name="perfis"),
     
     # Editar usuário (rota dinâmica com ID do usuário)
     path("editUser/<int:user_id>/", views.edit_user, name="editUser"),
     
     # Remover usuário (rota dinâmica com ID do usuário)
     path("deleteUser/<int:user_id>/", views.delete_user, name="deleteUser"),
+
+     path("produtos/", views.produtos, name="produtos"),
+
+     path("cadastro/", views.signup, name="cadastro"),
+
+     path("login/", views.login, name="login"),
 ]
--- estoque_app/views.py
+++ estoque_app/views.py
@@ -8,7 +8,7 @@ def home(request):
     Método executado quando o usuário está na interface inicial do sistema.
     Envia-se uma solicitação de renderização da interface home.html.
     """
-    return render(request, "estoque/home.html")
+    return render(request, "estoque/home copy.html")
 
 
 def create_user(request):
@@ -66,3 +66,16 @@ def delete_user(request, user_id):
         return redirect("home")  # Redireciona para a página inicial após deletar o usuário
 
     return render(request, "estoque/deleteUser.html", {"user": user})
+
+
+def perfis (request):
+    return render(request, "estoque/perfis.html")
+
+def produtos (request):
+    return render(request, "estoque/produtos.html")
+
+def signup (request):
+    return render(request, "estoque/signup.html")
+
+def login (request):
+    return render(request, "estoque/login.html")
